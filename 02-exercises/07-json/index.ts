210295cd5bc074e3224db594b5d081aa39b065b9
Mattias Kindborg <mattiask@axis.com>
2021-10-18 11:19:56 +0200

    fix(exercises/json): expected output

--- 02-exercises/07-json/index.ts
+++ 02-exercises/07-json/index.ts
@@ -3,11 +3,11 @@
 // objects.
 
 const toJson = (item: unknown): string => {
-    throw new Error("Not implemented")
+    return JSON.stringify(item)
 }
 
 const fromJson = (json: string): unknown => {
-    throw new Error("Not implemented")
+    return JSON.parse(json)
 }
 
 interface Person {
@@ -24,6 +24,6 @@ const json = toJson(person)
 console.log(json) // {"name":"John","age":42}
 
 person = fromJson(json) as Person
-console.log(person) // { name: "John", age: 42 }
+console.log(person) // { name: "John", age: 42 } or { name: 'John', age: 42 }
 
 export {}
